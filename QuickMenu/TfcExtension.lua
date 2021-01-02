69e73660a8b98b8b1141900589ded1e0f5748310
Tom Cumbow <tcumbow@icloud.com>
2021-01-01 22:57:27 -0500

    better icon for dismiss pets

--- QuickMenu/TfcExtension.lua
+++ QuickMenu/TfcExtension.lua
@@ -72,7 +72,7 @@ end
 
 function TfcQuickMenuExtraActions ()
     QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "ReloadUI", "Reload UI", "esoui/art/menubar/gamepad/gp_playermenu_icon_submitfeedback.dds", function() ReloadUI("ingame") end)
-    QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "DismissAllPets", "Dismiss All Pets", "EsoUI/Art/MenuBar/Gamepad/gp_playerMenu_icon_achievements.dds", DismissAllPets)
+    QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "DismissAllPets", "Dismiss All Pets", "esoui/art/emotes/gamepad/gp_emoteicon_emotion.dds", DismissAllPets)
     QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "TravelToLeader", "Travel To Group Leader", "EsoUI/Art/MenuBar/Gamepad/gp_playerMenu_icon_groups.dds", JumpToGroupLeader)
     QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "TravelToSpouse", "Travel To Spouse", "EsoUI/Art/MenuBar/Gamepad/gp_playerMenu_icon_contacts.dds", TravelToSpouse)
     QuickMenu.RegisterGamepadMenuEntry("TfcExtension", "LeaveDisbandGroup", "Leave/Disband Group", "EsoUI/Art/MenuBar/Gamepad/gp_playerMenu_icon_emotes.dds", LeaveDisbandGroup)
